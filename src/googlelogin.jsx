5c5e41f6eafead310f7c3e843d2b45bd0a809743
camilinmrtz <131947044+camilinmrtz@users.noreply.github.com>
2025-11-14 20:51:26 -0300

    Update googlelogin.jsx

--- src/googlelogin.jsx
+++ src/googlelogin.jsx
@@ -1,35 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function GoogleLogin() {
-  useEffect(() => 
-    const script = document.createElement("script");
-    script.src = "https://accounts.google.com/gsi/client";
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild(script);
-    script.onload = () => {
-  
-      window.google.accounts.id.initialize({
-        client_id: "876736803998-c86thjpgvlijqafimud2pskgurd7njfu.apps.googleusercontent.com",
-        callback: handleCredentialResponse, 
-      });
+  const [authUrl, setAuthUrl] = useState("");
 
-      
-      window.google.accounts.id.renderButton(
-        document.getElementById("googleButton"),
-        { theme: "outline", size: "large" } 
-      );
-
-    };
+  useEffect(() => {
+    fetch("http://localhost:3000/auth/url")
+      .then(res => res.json())
+      .then(data => setAuthUrl(data.url));
   }, []);
 
-
-  const handleCredentialResponse = (response) => {
-    console.log("Token JWT recibido:", response.credential);
-   
+  const handleLogin = () => {
+    window.location.href = authUrl; // redirige a Google
   };
 
-  return <div id="googleButton"></div>;
+  return (
+    <div>
+      <button onClick={handleLogin}>Login con Google</button>
+    </div>
+  );
 }
 
 export default GoogleLogin;
+
