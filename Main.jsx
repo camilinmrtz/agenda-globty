1772f34fec545ef68e3294cce3ef1c5cdaa420c0
camilinmrtz <131947044+camilinmrtz@users.noreply.github.com>
2025-11-14 20:40:40 -0300

    Update Main.jsx

--- Main.jsx
+++ Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // si tenés estilos
+import "./index.css"; // opcional, si tenés estilos
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
